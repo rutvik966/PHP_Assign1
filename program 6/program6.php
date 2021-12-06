d09cb137d604d9fc222411c8be64787a7f51888d
rutvik966 <86285948+rutvik966@users.noreply.github.com>
2021-12-06 21:38:11 +0530

    Update program6.php

--- program 6/program6.php
+++ program 6/program6.php
@@ -6,16 +6,19 @@ $c = $_POST['c'];
 $d = $_POST['b'];
 $e = $_POST['bs'];
 
-$cm = $a*$b*$c;
-$bo = $cm*b;
-$gs = $e+$cm+$bo;
+$cm = $a * $b * $c;
+$bonus = $cm * $b;
+$gs = $e + $cm + $bonus;
 
 //(hint: commission=quantity*price*commission rate 
 //bonus=commission*bonus rate
 // gross salary=commission+bonus+base salary)
 
 echo "Comission Is ".$cm;
-echo "Bonus Is ".$bo;
+echo "<br><br>";
+echo "Bonus Is ".$bonus;
+echo "<br><br>";
 echo "Gross Salary Is ".$gs;
+echo "<br><br>";
 
 ?>
